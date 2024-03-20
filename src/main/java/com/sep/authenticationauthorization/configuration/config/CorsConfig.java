281d0fceb738d497e1dac58b4932c90e94f74bc2
Charith Banneheka <charithbanneheka123@gmail.com>
2024-03-21 01:02:07 +0530

    CorsConfig class code update

--- src/main/java/com/sep/authenticationauthorization/configuration/config/CorsConfig.java
+++ src/main/java/com/sep/authenticationauthorization/configuration/config/CorsConfig.java
@@ -1,15 +1,23 @@
 package com.sep.authenticationauthorization.configuration.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration	
 public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200", // Angular app running on localhost
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200", // Angular app running on localhost
 				"http://travel-trek-angular-client.s3-website-ap-southeast-1.amazonaws.com" // Another example domain
-		).allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").allowedHeaders("*");
+						)
+				.allowedMethods(HttpMethod.GET.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.PUT.name(),
+						HttpMethod.DELETE.name(),
+						HttpMethod.PATCH.name())
+				.allowedHeaders("*");
 	}
 }
