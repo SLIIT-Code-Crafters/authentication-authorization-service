4bbc3f49702b74294eca327b8c4f071f84e088ca
Charith Banneheka <charithbanneheka123@gmail.com>
2024-03-22 01:23:22 +0530

    remove cross origin from controller

--- src/main/java/com/sep/authenticationauthorization/configuration/controller/AuthenticationController.java
+++ src/main/java/com/sep/authenticationauthorization/configuration/controller/AuthenticationController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +28,7 @@ import com.sep.authenticationauthorization.configuration.service.AuthenticationS
 import com.sep.authenticationauthorization.configuration.service.MasterTokenService;
 import com.sep.authenticationauthorization.configuration.utill.CommonUtils;
 
-@CrossOrigin("*")
+//@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/private/auth")
 public class AuthenticationController {
