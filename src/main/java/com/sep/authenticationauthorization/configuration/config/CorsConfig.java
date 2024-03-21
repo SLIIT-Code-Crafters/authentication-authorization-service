a287786e939583cf6f3f206ebec8364c38a76248
Charith Banneheka <charithbanneheka123@gmail.com>
2024-03-22 01:21:15 +0530

    update cors code

--- src/main/java/com/sep/authenticationauthorization/configuration/config/CorsConfig.java
+++ src/main/java/com/sep/authenticationauthorization/configuration/config/CorsConfig.java
@@ -1,23 +1,27 @@
 package com.sep.authenticationauthorization.configuration.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration	
 public class CorsConfig implements WebMvcConfigurer {
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//				.allowedOrigins("*")
+//				.allowedMethods(HttpMethod.GET.name(),
+//						HttpMethod.POST.name(),
+//						HttpMethod.PUT.name(),
+//						HttpMethod.DELETE.name(),
+//						HttpMethod.PATCH.name())
+//				.allowedHeaders("*");
+//	}
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:4200", // Angular app running on localhost
-				"http://travel-trek-angular-client.s3-website-ap-southeast-1.amazonaws.com" // Another example domain
-						)
-				.allowedMethods(HttpMethod.GET.name(),
-						HttpMethod.POST.name(),
-						HttpMethod.PUT.name(),
-						HttpMethod.DELETE.name(),
-						HttpMethod.PATCH.name())
-				.allowedHeaders("*");
-	}
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
