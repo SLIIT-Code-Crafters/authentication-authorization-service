771d8a1d096980cf7fe2cbcea59114f64ccb1651
Charith Banneheka <charithbanneheka123@gmail.com>
2024-04-15 13:48:06 +0530

    updated the time zone v2

--- src/main/java/com/sep/authenticationauthorization/AuthenticationAuthorizationServiceApplication.java
+++ src/main/java/com/sep/authenticationauthorization/AuthenticationAuthorizationServiceApplication.java
@@ -1,5 +1,10 @@
 package com.sep.authenticationauthorization;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class AuthenticationAuthorizationServiceApplication {
 
+	@Value("${tsms.default.timeZone}")
+	public String timeZone;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationAuthorizationServiceApplication.class, args);
 	}
@@ -15,6 +23,11 @@ public class AuthenticationAuthorizationServiceApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
 	}
 
 }
